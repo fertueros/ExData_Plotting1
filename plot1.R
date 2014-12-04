06326ce00c2fa05ee715757f8ab3ff62a5416e42
Fernando <fertueros@gmail.com>
2014-12-03 20:55:55 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,5 +2,5 @@ source('cargar.R')# script for load the data
 png(filename="plot1.png", width=480, height=480)# set the image file with width and height
 hist(datos$Global_active_power, col="red", 
      xlab="Global Active Power (kilowatts)", 
-     main="Global Active Power")
+     main="Global Active Power")# making a histogram with base plot system
 dev.off()# close the device
