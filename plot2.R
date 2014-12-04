d0a0f9c6eefc6d27b50dd0296766989d429aa54c
Fernando <fertueros@gmail.com>
2014-12-03 20:58:56 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
-source('cargar.R')
-png(filename = "plot2.png", width = 480, height = 480)
+source('cargar.R')# script for load the data
+png(filename = "plot2.png", width = 480, height = 480)# seting the image file with width and hight
 plot(datos$DateTime, datos$Global_active_power, type="l", xlab="", 
-     ylab="Global Active Power (kilowatts)")
-dev.off()
+     ylab="Global Active Power (kilowatts)")# making a line graph with base plot system
+dev.off()# close the device
