d4e541ab72c8592ad26236fd2155b8b872955715
Fernando <fertueros@gmail.com>
2014-12-03 20:53:27 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
-source('cargar.R')
-png(filename="plot1.png", width=480, height=480)
+source('cargar.R')# script for load the data
+png(filename="plot1.png", width=480, height=480)# set the image file with width and height
 hist(datos$Global_active_power, col="red", 
      xlab="Global Active Power (kilowatts)", 
      main="Global Active Power")
-dev.off()
+dev.off()# close the device
